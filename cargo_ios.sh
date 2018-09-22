a711ecb69b46b6fc34e3d2b467eba378b6319666
tyler <tyler@brainiumstudios.com>
2018-09-22 12:06:31 -0700

    add rust code to ios project for debugging

--- cargo_ios.sh
+++ cargo_ios.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+# Iterate through the list of ARCHS and build each one
 for ARCH in `echo $ARCHS`; do
     RUST_ARCH="${ARCH}-apple-ios"
+    # iOS and rust differ on the name of this ARCH
     if [ "${ARCH}" = "arm64" ]; then
         RUST_ARCH="aarch64-apple-ios"
     fi
 
+    # build in release mode if the configuration requests it
     if [ "${CONFIGURATION}" = "release" ]; then
         CONFIG="--release"
     fi
